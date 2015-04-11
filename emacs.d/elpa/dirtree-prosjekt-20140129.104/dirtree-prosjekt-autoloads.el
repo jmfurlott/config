473fc8267b87f52ddc969c7135c4f2c157bf8677
Joseph Furlott <jmfurlott@gmail.com>
2015-04-11 11:50:54 -0400

    Removed old

--- emacs.d/elpa/dirtree-prosjekt-20140129.104/dirtree-prosjekt-autoloads.el
+++
@@ -1,22 +0,0 @@
-;;; dirtree-prosjekt-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
-
-;;;### (autoloads nil "dirtree-prosjekt" "dirtree-prosjekt.el" (21574
-;;;;;;  28168 0 0))
-;;; Generated autoloads from dirtree-prosjekt.el
-
-(autoload 'prosjekt-dirtree "dirtree-prosjekt" "\
-
-
-\(fn)" t nil)
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; dirtree-prosjekt-autoloads.el ends here
--- emacs.d/elpa/grizzl-20130810.202/grizzl-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "grizzl" "20130810.202" "Fuzzy Search Library & Completing Read"
-  '((cl-lib "0.1")))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
